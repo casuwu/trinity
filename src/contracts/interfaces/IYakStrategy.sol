542d3262f09f30b52312249f1b8b6bcdd3785826
concave-engineering <97050605+concave-engineering@users.noreply.github.com>
2023-04-03 10:21:43 -0400

    clean

--- src/contracts/interfaces/IYakStrategy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-
-interface IYakStrategy {
-    function depositToken() external view returns (address);
-
-    function depositFor(address account, uint256 amount) external;
-}
