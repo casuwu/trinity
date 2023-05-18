7f2c70eb2097f8a77792a4ee7ff323adb722d72e
concave-engineering <taken404@gmail.com>
2023-05-18 13:45:20 -0400

    add method to convert out of scientific notation

--- pkg/p/pcnv.py
+++ pkg/p/pcnv.py
@@ -6,6 +6,19 @@ def csv_to_string(file_path):
         data = file.read()
     return data
 
+def convert_scientific_to_numeric(df, column):
+    # Identify the numeric columns
+    numeric_columns = df.select_dtypes(include=['float64', 'float32', 'int64', 'int32']).columns
+
+    # Check if the specified column is numeric
+    if column not in numeric_columns:
+        raise ValueError(f"Column '{column}' is not numeric.")
+
+    # Convert scientific notation to numeric
+    df[column] = df[column].apply(lambda x: f'{x:.0f}' if pd.notnull(x) else x)
+
+    return df
+
 csv_string = csv_to_string('./holders.csv')
 
 # convert string to csv file like object
@@ -16,5 +29,13 @@ df = pd.read_csv(data)
 
 # convert dataframe to json
 json = df.to_json(orient='records')
+# Iterate over each element in the "Owed" column and convert the values
+df = convert_scientific_to_numeric(df, 'Owed')
 
-print(json)
+# Display the modified DataFrame
+print(df)
+
+
+
+
+
